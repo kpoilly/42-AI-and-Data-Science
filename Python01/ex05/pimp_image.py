d72d9fa100df3ecede6c9a9f49bf3d571f0ee5f7
Kilyan Poilly <kpoilly@paul-f4Br5s3.clusters.42paris.fr>
2025-02-14 13:44:36 +0100

    Python01:ex05 WIP

---
+++ Python01/ex05/pimp_image.py
@@ -0,0 +1,47 @@
+import sys
+import cv2
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from load_image import ft_load
+matplotlib.use('gtk3agg')
+
+
+def ft_invert(array) -> np.array:
+    """
+    Inverts the color of the image received.
+    """
+    # = + - *
+    pass
+
+
+def ft_red(array) -> np.array:
+    """
+    Converts the color of the image received to shades of red.
+    """
+    # = *
+    pass
+
+
+def ft_green(array) -> np.array:
+    """
+    Converts the color of the image received to shades of green.
+    """
+    # = -
+    pass
+
+
+def ft_blue(array) -> np.array:
+    """
+    Converts the color of the image received to shades of blue.
+    """
+    # =
+    pass
+
+
+def ft_grey(array) -> np.array:
+    """
+    Converts the color of the image received to shades of grey.
+    """
+    # = /
+    pass
