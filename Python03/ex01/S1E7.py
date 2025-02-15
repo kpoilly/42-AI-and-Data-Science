adac0c4582c9ba9b9c5bb468bbdf2db98f79e9be
Kilyan <poillykilyan@gmail.com>
2025-02-15 17:48:56 +0100

    Python03:ex01 WIP

---
+++ Python03/ex01/S1E7.py
@@ -0,0 +1,14 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+	def __init__():
+		pass
+
+
+class Lannister(Character):
+	def __init__():
+		pass
+	# decorator
+	def create_lannister(your code here):
+		pass
