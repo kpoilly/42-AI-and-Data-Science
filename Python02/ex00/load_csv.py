d9a1d24cf59082259b4cbc9f90278aa95b766fa6
Kilyan <poillykilyan@gmail.com>
2025-02-15 16:21:50 +0100

    Python02:ex00 normed

--- Python02/ex00/load_csv.py
+++ Python02/ex00/load_csv.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 
 def load(path: str) -> pd.array:
+    if (".csv" not in path):
+        return None
     try:
         dataset = pd.read_csv(path)
     except FileNotFoundError:
-        raise AssertionError(f"{path} could not be loaded.\
- (is path correct ?)")
+        return None
     print("Loading dataset of dimensions", dataset.shape)
     return dataset
