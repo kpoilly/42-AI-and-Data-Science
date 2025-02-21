5998c7f41d21183af65b58395e9ecb63f84e256a
Kilyan Poilly <kpoilly@made-f0Dr9s9.clusters.42paris.fr>
2025-02-21 19:55:27 +0100

    Python04:03

---
+++ Python04/ex03/new_student.py
@@ -0,0 +1,24 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    """
+Dataclass representing a student.
+args:
+    name, surname
+    """
+    name: str = field(init=True)
+    surname: str = field(init=True)
+    active: bool = field(default=True)
+    login: str = field(init=False)
+    id: str = field(init=False, default=generate_id())
+
+    def __post_init__(self):
+        self.login = self.name[0] + self.surname
