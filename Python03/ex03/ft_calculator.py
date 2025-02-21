050187b64397e987bb846fb41cab1c60d7c75b72
Kilyan Poilly <kpoilly@made-f0Dr9s9.clusters.42paris.fr>
2025-02-21 16:53:17 +0100

    Python03:ex03

--- Python03/ex03/ft_calculator.py
+++ Python03/ex03/ft_calculator.py
@@ -1,10 +1,28 @@
-class calculator:
+import numpy as np
 
-	def __add__(self, object) -> None:
-		# your code here
-	def __mul__(self, object) -> None:
-		# your code here
-	def __sub__(self, object) -> None:
-		# your code here
-	def __truediv__(self, objject) -> None:
-		
+
+class calculator:
+    """
+A class representing a calculator able to do calculations
+of vector with a scalar.
+Methods:
+    +, *, -, /
+    """
+    def __init__(self, vector):
+        self.vector = np.array(vector)
+
+    def __add__(self, object) -> None:
+        self.vector += object
+        print(self.vector)
+
+    def __mul__(self, object) -> None:
+        self.vector *= object
+        print(self.vector)
+
+    def __sub__(self, object) -> None:
+        self.vector -= object
+        print(self.vector)
+
+    def __truediv__(self, object) -> None:
+        self.vector /= object
+        print(self.vector)
