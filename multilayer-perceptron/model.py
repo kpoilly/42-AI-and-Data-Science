9454b3d1e94e3e03239d943735b675e2333abeb5
Kilyan <poillykilyan@gmail.com>
2025-02-24 23:51:03 +0100

    WIP

--- multilayer-perceptron/model.py
+++ multilayer-perceptron/model.py
@@ -1,10 +1,14 @@
 import sys
 import numpy as np
 
+
+def softmax(values):
+    return np.exp(values) / np.sum(np.exp(values))
+
 class DenseLayer:
-	"""
+    """
  class representing a layer of the multilayer perceptron.
-	"""
+    """
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -14,9 +18,9 @@ class DenseLayer:
 
 
 class ReLU:
-	"""
+    """
  class representing the ReLU activation function.
-	"""
-	@staticmethod
-	def forward(self, inputs):
-		self.output = np.maximum(0, inputs)
+    """
+    @staticmethod
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
--- multilayer-perceptron/train.py
+++ multilayer-perceptron/train.py
@@ -1,8 +1,13 @@
 import sys
+import math
 import argparse
 import numpy as np
 from utils import load_data
-from model import
+from model import DenseLayer
+
+
+def train(network, lr, batch_size, epochs):
+    pass    
 
 
 def main():
@@ -11,11 +16,19 @@ def main():
         print("Error: Cannot found data_train.csv\nDid you separate the data file first?", file=sys.stderr)
         return 1
 
-    parser = argparse.ArgumentParser(description="Model parameters")
-    parser.add_argument('--layers', type=int, default=24, choices=range(0, 128), help="Numbers of neurons per layer")
+    parser = argparse.ArgumentParser(description="Training parameters")
+    parser.add_argument('--layers', type=int, default=2, choices=range(0, 50), help="Numbers of hidden layers between input and output layer")
     parser.add_argument('--epochs', type=int, default=100, choices=range(0, 10000), help="Numbers of epochs")
-    parser.add_argument('--lr', type=float, default=0.01, choices=range(0, 1), help="Learning rate")
+    parser.add_argument('--lr', type=float, default=0.01, choices=range(0, 10), help="Learning rate")
     parser.add_argument('--batch_size', type=int, default=32, choices={12, 24, 32, 64, 128}, help="Batch size")
+    args = parser.parse_args()
+    
+    network = [DenseLayer(X.shape[0], 12)] # input layer
+    for i in range(0, args.layers):
+        network.append(DenseLayer(12, 12))
+    network.append(DenseLayer(12, 2)) # output layer
+    
+    train(network, args.lr, args.batch_size, args.epochs)
 
 
 if __name__ == "__main__":
