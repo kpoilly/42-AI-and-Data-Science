44d0640e26fd4e2ff9f2c4e858c969aecaa2a0dd
Kilyan <poillykilyan@gmail.com>
2025-02-24 19:07:20 +0100

    WIP

--- multilayer-perceptron/utils.py
+++ multilayer-perceptron/utils.py
@@ -16,3 +16,12 @@ def load_data(path):
         return None, None
     except IsADirectoryError:
         return None, None
+    
+def normalize_data(data):
+    """
+    Normalize the dataset
+    """
+    mean = np.mean(data, axis=0)
+    std_dev = np.std(data, axis=0)
+    norm_data = (data - mean) / std_dev
+    return norm_data, mean, std_dev
