af19852fc6b2a29e1f3c929ae4d2d08f9594bf4a
Kilyan <poillykilyan@gmail.com>
2025-02-24 17:03:11 +0100

    Python04:ex02

--- Python04/ex02/callLimit.py
+++ Python04/ex02/callLimit.py
@@ -1,7 +1,20 @@
+import sys
+
+
 def callLimit(limit: int):
     count = 0
 
     def callLimiter(function):
 
         def limit_function(*args, **kwds):
-            nonlocal count
+            try:
+                nonlocal count
+                count += 1
+                if count <= limit:
+                    return function(*args, **kwds)
+                else:
+                    raise AssertionError(f"{function} called too many times.")
+            except AssertionError as e:
+                print("AssertionError:", e, file=sys.stderr)
+        return limit_function
+    return callLimiter
