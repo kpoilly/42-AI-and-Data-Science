5e3c704eccb3c344ff6f1b73a87ac6eaf1653443
Kilyan Poilly <kpoilly@bess-f2r1s10.clusters.42paris.fr>
2025-03-18 18:33:14 +0100

    push

---
+++ Piscine Data Science/Data Scientist part 1/ex00/Histrogram.py
@@ -0,0 +1,63 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from load_csv import load
+
+
+def draw_single(path):
+    try:
+        df = load(path)
+    except:
+        print("Error loading csv file.", file=sys.stderr)
+        exit()
+    
+    nb_cols = len(df.columns)
+    nb_rows = (nb_cols) // 5
+    
+    fig, axes = plt.subplots(nrows=nb_rows, ncols=min(nb_cols, 5), figsize=(15, 2.5 * nb_rows))
+    axes = axes.flatten()
+    
+    for i, column in enumerate(df.columns):
+        if i < len(axes):
+            axes[i].hist(df[column], bins=40, color='darkseagreen', label='Knight')
+            axes[i].set_title(column)
+            axes[i].legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.savefig(f"Histrogram_single.jpg")
+    
+def draw_double(path):
+    try:
+        df = load(path)
+    except:
+        print("Error loading csv file.", file=sys.stderr)
+        exit()
+
+    graphs_name = [col for col in df.columns if col != 'knight']
+    nb_cols = len(graphs_name)
+    nb_rows = (nb_cols) // 5
+    
+    fig, axes = plt.subplots(nrows=nb_rows, ncols=min(nb_cols, 5), figsize=(15, 2.5 * nb_rows))
+    axes = axes.flatten()
+    
+    for i, column in enumerate(df.columns):
+        if i < len(axes):
+            subset_jedi = df[df['knight'] == 'Jedi'][column]
+            axes[i].hist(subset_jedi, bins=40, color='blue', alpha=0.5, label='Jedi')
+            
+            subset_sith = df[df['knight'] == 'Sith'][column]
+            axes[i].hist(subset_sith, bins=40, color='tomato', alpha=0.5, label='Sith')
+            
+            axes[i].set_title(column)
+            axes[i].legend()
+
+    plt.tight_layout()
+    plt.savefig("Histrogram_double.jpg")
+
+def main():
+    draw_single("Test_knight.csv")
+    draw_double("Train_knight.csv")
+
+if __name__ == "__main__":
+    main()
