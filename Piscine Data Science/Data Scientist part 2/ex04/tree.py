e7be14596552ac6ef38b899aa4688d2700de2b4f
Kilyan Poilly <kpoilly@paul-f4Br8s2.clusters.42paris.fr>
2025-03-19 18:39:07 +0100

    WIP:ex04 - Norm++

--- Piscine Data Science/Data Scientist part 2/ex04/tree.py
+++ Piscine Data Science/Data Scientist part 2/ex04/tree.py
@@ -1,8 +1,5 @@
 import sys
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
@@ -11,7 +8,10 @@ from load_csv import load
 
 def predict(path_train, path_val):
     """
-    Blablabla
+    Uses DecisionTreeClassifier
+    Prints F1-Score (Needs to be >90%)
+    Display the tree in a graph.jpg
+    Predictions in tree.txt
     """
     try:
         df_train = load(path_train)
@@ -20,29 +20,30 @@ def predict(path_train, path_val):
         print(f"Error: {str(e)}", file=sys.stderr)
         exit()
 
-    #Data prep
+    # Data prep
     mapping = {'Jedi': 1, 'Sith': 0}
     y_train = df_train['knight'].map(mapping)
     X_train = df_train.drop(columns=['knight'])
     y_val = df_val['knight'].map(mapping)
     X_val = df_val.drop(columns=['knight'])
-    
-    #Model training
+
+    # Model training
     model = DecisionTreeClassifier(random_state=42)
     model.fit(X_train, y_train)
-    
-    #F1 Calculation
+
+    # F1 Calculation
     prediction = model.predict(X_val)
     f1 = f1_score(y_val, prediction)
     print(f"F1-score on data_validation : {round(f1, 5) * 100}%")
-    
-    #Display tree in a graph
+
+    # Display tree in a graph
     plt.figure(figsize=(12, 8))
-    plot_tree(model, feature_names=X_train.columns, class_names=['Sith', 'Jedi'], filled=True)
+    plot_tree(model, feature_names=X_train.columns,
+              class_names=['Sith', 'Jedi'], filled=True)
     plt.savefig("graph.jpg")
     print("Tree displayed in 'graph.jpg'")
-    
-    #Tree.txt file
+
+    # Tree.txt file
     reverse_mapping = {1: 'Jedi', 0: 'Sith'}
     prediction = [reverse_mapping[pred] for pred in prediction]
     with open("Tree.txt", "w") as f:
