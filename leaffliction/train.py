e4cfc5e034d1ad90c1d1f3f3d02413f20d165c58
Kilyan <poillykilyan@gmail.com>
2025-03-23 17:42:41 +0100

    WIP: Model created, starting building training routine

--- leaffliction/train.py
+++ leaffliction/train.py
@@ -12,33 +12,33 @@ from tensorflow.keras.utils import image_dataset_from_directory
 
 
 def create_model(nb_outputs, nb_filters=64, dropout=0.5):
-    model = model.Sequential()
+    model = models.Sequential()
     model.add(layers.Rescaling(1.0 / 255))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(nb_filters, (3, 3), activation="ReLU"))
+    model.add(layers.Conv2D(nb_filters, (3, 3), activation="relu"))
     model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(nb_filters, (3, 3), activation="ReLU"))
+    model.add(layers.Conv2D(nb_filters, (3, 3), activation="relu"))
     model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(32, (1, 1), activation="ReLU"))
+    model.add(layers.Conv2D(32, (1, 1), activation="relu"))
     model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.Flatten())
-    model.add(layers.Dense(512, activation="ReLU"))
+    model.add(layers.Dense(512, activation="relu"))
     model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(256, activation="ReLU"))
-    model.add(layers.Dense(nb_outputs, activation="Softmax"))
+    model.add(layers.Dense(256, activation="relu"))
+    model.add(layers.Dense(nb_outputs, activation="softmax"))
     model.compile(
-        optimizer="ADAM",
+        optimizer="adam",
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=["accuracy"],
     )
     return model
 
 
-def train(df, df_val, nb_filters=64, dropout=0.5, epochs=100):
+def train(df, df_val, nb_filters=64, dropout=0.5, epochs=10):
     print(f"Starting model's training with Settings :\n{epochs} epochs\n\
-          Convolution filters: {nb_filters}\nDropout: {dropout}")
+Convolution filters: {nb_filters}\nDropout: {dropout}")
     model = create_model(len(df.class_names), nb_filters, dropout)
     model.fit(df, epochs=epochs, validation_data=df_val)
 
@@ -49,10 +49,10 @@ def train(df, df_val, nb_filters=64, dropout=0.5, epochs=100):
 
 def main():
     # Replace with argparse
-    df_train_path = "./data/Apple_scab"
-    df_val_path = "./data/Apple_scab"
+    df_train_path = "data"
+    df_val_path = "data"
     
-    # Add nb epochs, nb filters, dropout and other
+    # Add batch_size, nb epochs, nb filters, dropout and other
     # training settings with argparse
     
     try:
@@ -62,7 +62,7 @@ def main():
         print(f"Error: {str(e)}", file=sys.stderr)
         exit()
     
-    train(df_train, df_val, 64, 0.5, 100)
+    train(df_train, df_val, 64, 0.5, 2)
 
 
 if __name__ == "__main__":
--- leaffliction/utils.py
+++ leaffliction/utils.py
@@ -6,8 +6,8 @@ def load(path: str):
     Loads an image and returns it as a tf.Dataset
     """
     try:
-        dataset = image_dataset_from_directory(path, image_size=(128, 128), suffle=True)
+        dataset = image_dataset_from_directory(path, image_size=(128, 128), shuffle=True)
     except FileNotFoundError:
         raise AssertionError(f"file {path} not found.")
-    print("Loading dataset of dimensions", dataset.shape)
+    print("Loading dataset of dimensions", dataset.element_spec)
     return dataset
