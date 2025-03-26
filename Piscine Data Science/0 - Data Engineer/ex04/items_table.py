4fdfb20c99ee5177a4c1ed4197057a08a62fe686
Kilyan Poilly <kpoilly@paul-f4Br7s2.clusters.42paris.fr>
2025-03-26 12:39:43 +0100

    complete

--- Piscine Data Science/0 - Data Engineer/ex04/items_table.py
+++ Piscine Data Science/0 - Data Engineer/ex04/items_table.py
@@ -3,7 +3,6 @@ import sys
 import pandas as pd
 from sqlalchemy import create_engine, Table, Column, \
      Integer, String, Float, DateTime, MetaData
-from datetime import datetime
 
 
 DATABASE_URL = "postgresql://kpoilly:mysecretpassword@localhost:5432/piscineds"
@@ -23,7 +22,11 @@ def csv_to_table(engine, csv_path):
 
         columns = []
         columns_types = {}
-        columns.append(Column('Datetime', DateTime, default=datetime.utcnow))
+
+        df['Datetime'] = str(pd.Timestamp('now'))
+        cols = df.columns.tolist()
+        cols = ['Datetime'] + [col for col in cols if col != 'Datetime']
+        df = df[cols]
 
         for col in df.columns:
             try:
@@ -46,6 +49,7 @@ def csv_to_table(engine, csv_path):
 
         Table(tablename, metadata, *columns)
         metadata.create_all(engine)
+        df.to_sql(tablename, engine, if_exists='append', index=False)
         print(f"Table {tablename} created from {filename}.")
 
     except Exception as e:
