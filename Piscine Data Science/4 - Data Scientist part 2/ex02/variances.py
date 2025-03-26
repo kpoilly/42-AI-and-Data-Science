bcecdc3a08b372aa58e16a27099bb553b83d593c
Kilyan Poilly <kpoilly@paul-f4Br7s2.clusters.42paris.fr>
2025-03-26 17:03:02 +0100

    WIP: ex02: init

---
+++ Piscine Data Science/4 - Data Scientist part 2/ex02/variances.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    variances = []
+    cumul_var = []
+
+    print(f"Variances (Percentage):{variances}\n")
+    print(f"Cumulative Variances (Percentage):{cumul_var}")
+
+
+if __name__ == "__main__":
+    main()
