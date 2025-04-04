7d4e1da7072ae4c31f20740927f5b30ea5c5fd21
Kilyan Poilly <kpoilly@paul-f5Ar3s1.clusters.42paris.fr>
2025-04-04 14:30:37 +0200

    WIP: ex02

--- Piscine Data Science/1 - Data Warehouse/ex02/remove_duplicates.py
+++ Piscine Data Science/1 - Data Warehouse/ex02/remove_duplicates.py
@@ -10,27 +10,37 @@ WITH RowNumCTE AS (
         customers
 )
 DELETE FROM customers
-WHERE rn > 1;
+WHERE (
+    {columns_list}
+) IN (
+    SELECT
+        {columns_list}
+    FROM
+        RowNumCTE
+    WHERE
+        rn > 1
+);
 """
 
 
 def main():
+    print("Deleting duplicate rows from customers...")
     try:
-        engine = create_engine('postgresql://kpoilly:mysecretpassword@localhost:5432/piscineds')
+        engine = create_engine("postgresql://kpoilly:mysecretpassword@localhost:5432/piscineds")
         inspector = inspect(engine)
         columns = inspector.get_columns('customers')
-        column_names = ['customers.' + col['name'] for col in columns]
+        column_names = [f'"{col["name"]}"' for col in columns]
         columns_list = ', '.join(column_names)
 
         with engine.connect() as connection:
             connection.execute(text(query.format(columns_list=columns_list)))
             connection.commit()
-            print("Duplicate raows deleted from customers")
+            print("Duplicate rows deleted from customers")
     except Exception as e:
         print(f"Error: {str(e)}")
         exit()
     engine.dispose()
-    
+
 
 if __name__ == "__main__":
     main()
